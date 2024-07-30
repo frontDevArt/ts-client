d9693b1f19db18c80b3d9be3594841ae124b7cbe
Artur Gevorgyan <frontdevart@gmail.com>
2024-07-30 23:48:28 +0400

    new design

--- src/components/NewApp/Right/Right.jsx
+++ src/components/NewApp/Right/Right.jsx
@@ -3,16 +3,25 @@ import './Right.scss'
 const Right = () => {
   return (
     <div className='right'>
-      <h2 className='right__title'>WE’RE MAKING IMPROVEMENTS TO OUR SECURITY.</h2>
-      <p className="right__text right__text--bold">Your account is about to get more secure.</p>
-      <p className="right__text">To help keep your account safe, we are requiring Multi-Factor Authentication (MFA).</p>
-      <p className="right__text">When you attempt to log into your account from a new or unrecognized device, you will be asked to verify your identity. You can choose your preferred verification method, including a:</p>
+      <h2 className='right__title'>We're focused on removing fraud from our platform.</h2>
+      <div className='right__divider' />
+      <p className="right__subheading">How it works</p>
+      <p className="right__tsx-body-1">When you attempt to log in to your account from a new or unrecognized device, you will be asked to verify your identity.</p>
       <ul className='right__list'>
-        <li className='right__list-item'>Push notification on your mobile device</li>
-        <li className='right__list-item'>Security code via an authentication app</li>
-        <li className='right__list-item'>Biometric input such as your face or fingerprint</li>
+        <li>Push notification on your mobile device</li>
+        <li>Security code via an authentication app</li>
+        <li>Biometric input such as your face or fingerprint</li>
       </ul>
       <a className="right__link" target="_blank" rel="noopener noreferrer" href="https://truckstop.com/contact-us/">Need more help? Get support.</a>
+      <div className='right__divider' />
+      <p className="right__subheading">What customers are saying</p>
+      <div className="right__card">
+        <p className="right__quote">“Working with Truckstop.com, you get to know people from the CEO all  the way to IT. They’ve tailored their tools to work with us so they don’t have just one way of doing things.”</p>
+      </div>
+      <div className="flex">
+        <p className="right__quote-attribution">Zachary Green, Ryder</p>
+        <img alt="Person Icon" src="/img/icon_account.svg" />
+      </div>
     </div>
   )
 }
